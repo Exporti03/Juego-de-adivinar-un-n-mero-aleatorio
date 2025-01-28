4e406bb49faf4424bb1e066834895df3323f0f82
exporti03 <adzuluaga@outlook.com>
2025-01-28 16:06:57 -0500

    actualizar

--- juegodeadivinansa.cpp
+++ juegodeadivinansa.cpp
@@ -2,57 +2,76 @@
 #include <cstdlib>
 #include <ctime>
 
-
-
 int main() {
+    std::cout << "\t##### BIENVENIDO AL JUEGO DE ADIVINA TU NUMERO #####\n\n";
+    std::cout << "Tienes que adivinar un numero del 1 al 100.\n"
+              << "Las oportunidades dependen del nivel que escojas.\n\n";
 
-    std::cout << "\t##### BIENVENIDO AL JUEGO DE ADIVINA TU NUMERO ##### \t\n";
+    int dificultad;
+    
+    while(true) {
+        std::cout << "\t### Marca el nivel de dificultad ###\n\n";
+        std::cout << "(1) Nivel facil  - 10 intentos\n";
+        std::cout << "(2) Nivel medio  - 7 intentos\n";
+        std::cout << "(3) Nivel dificil - 5 intentos\n";
+        std::cout << "(0) Salir del juego\n\n";
 
-    std::cout << "Tu tienes que escoger un numero del 1 al 100. " "tienes 11 oportunidades para adivinar el numero y todo depende del nivel que escojas. \n ";
+        std::cout << "Ingresa el nivel: ";
+        std::cin >> dificultad;
 
-int dificultad;
+        if (dificultad == 0) {
+            std::cout << "¡Gracias por jugar!\n";
+            break;
+        }
 
-while(true){
+        if (dificultad < 0 || dificultad > 3) {
+            std::cout << "Nivel invalido. Por favor, elige un nivel valido.\n\n";
+            continue;
+        }
 
-    std::cout << "\t### Marca el nivel de la dificultad ###\n\t";
+        // Inicializar número aleatorio
+        srand(time(0));
+        int numerosecreto = 1 + (rand() % 100);
+        int intentos;
 
-    std::cout << "\n(1 nivel facil!)\n";
-    std::cout << "(2 nivel medio)\n";
-    std::cout << "(3 nivel dificil)\n";
-    std::cout << "(0 salir del juego!)\n";
+        // Establecer número de intentos según dificultad
+        switch(dificultad) {
+            case 1: intentos = 10; break;
+            case 2: intentos = 7; break;
+            case 3: intentos = 5; break;
+        }
 
-    std::cout << "Ingresa el nivel: ";
-    std::cin >> dificultad;
+        std::cout << "\nTienes " << intentos << " intentos para adivinar el numero.\n";
 
-    srand(time(0));
-    int numerosecreto = 1 + (rand() % 100 );
-    int juegochoice;
+        // Bucle principal del juego
+        bool adivinado = false;
+        for (int i = 1; i <= intentos && !adivinado; i++) {
+            int numeroUsuario;
+            std::cout << "\nIntento " << i << "/" << intentos << ". Ingresa un numero: ";
+            std::cin >> numeroUsuario;
 
-    //dificultad facil
+            if (numeroUsuario == numerosecreto) {
+                std::cout << "\n¡FELICITACIONES! ¡Has adivinado el numero " << numerosecreto << "!\n";
+                adivinado = true;
+            } else {
+                if (numeroUsuario < numerosecreto) {
+                    std::cout << "El numero es mayor que " << numeroUsuario << "\n";
+                } else {
+                    std::cout << "El numero es menor que " << numeroUsuario << "\n";
+                }
 
-    if (dificultad == 1){
-        std::cout << "\n tus oportunidades son 10, de 1 a 100";
+                if (i < intentos) {
+                    std::cout << "Te quedan " << (intentos - i) << " intentos.\n";
+                }
+            }
+        }
+
+        if (!adivinado) {
+            std::cout << "\nSe acabaron los intentos. El numero era: " << numerosecreto << "\n";
+        }
+
+        std::cout << "\n¿Quieres jugar otra vez?\n\n";
     }
-
-    int vidasjugador = 10;
-    for (int i = 1; i <= 10; i++) {
-
-        std::cout <<"\n\nIngresa el numero: ";
-        std::cin >> juegochoice;
-    if (juegochoice == numerosecreto){
-        std::cout << "A GANADO!!" <<juegochoice << "Este es el numero secreto";
-        
-
-
-    }
-
-    }
-
-    break;
-
-
-}
-
 
     return 0;
 }
